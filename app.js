3d19ba3d865cde56c109002d84a92f2a03c022b2
Ubuntu <ubuntu@ip-172-31-7-29.ec2.internal>
2026-05-07 11:52:29 +0000

    updated message

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const http = require('http')
-const server = http.createServer(( req, res) => {
-	res.end("Hello from Docker CI/CD")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain; charset=utf-8'
+  });
+
+  res.end('Hello from Automated CI/CD Deployment\n');
 });
 
 server.listen(3000, () => {
-	console.log("Server running on port 3000")
+  console.log('Server running on port 3000');
 });
